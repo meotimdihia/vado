4a0e6138d02f92121309d718ba01faa574e20898
meotimdihia <meotimdihia@gmail.com>
2014-06-02 17:46:16 +0700

    init

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt-get update
+#apt-get install -y apache2
+#rm -rf /var/www
+#ln -fs /vagrant /var/www
+
+apt-get install -y curl
+apt-get install -y vim
+apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
+
+curl -s https://get.docker.io/ubuntu/ | sudo sh
