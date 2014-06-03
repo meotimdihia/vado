0562eb2a555c6f47b0507fca33ce8332f9cd5045
meotimdihia <meotimdihia@gmail.com>
2014-06-03 17:49:44 +0700

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,5 @@ apt-get install -y vim
 apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 curl -s https://get.docker.io/ubuntu/ | sudo sh
+
+#docker run -v /home/vagrant/mobplf/:/var/www -p 8080:80 -d apache
